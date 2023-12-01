bb446b7a4261f13cd39070ef7932db52c9aadaf9
Zakharov Yan <banjumingray@bk.ru>
2023-12-01 23:08:20 +0700

    пакет rest-клиент

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+REQUIRES = [
+    'requests',
+    'structlog',
+    'curlify',
+    'allure-pytest'
+]
+
+setup(
+    name='restclient',
+    version='0.0.1',
+    packages=['restclient'],
+    url='https://github.com/zakharovyn/restclient.git',
+    license='MIT',
+    author='yanzaharov',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='restclient with allure and logger'
+)
